805d1b210792e40c2e2d7b8f43377dfc40bd88b7
chaitanya kumar <nunna.chaitanyakumar.imp@gmail.com>
2025-11-13 14:24:52 +0530

    Switch to HashRouter for GitHub Pages routing fix

--- src/main.jsx
+++ src/main.jsx
@@ -13,14 +13,31 @@
 
 
 // main.jsx
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { BrowserRouter } from 'react-router-dom';
+// import App from './App';
+// import './index.css';
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <BrowserRouter basename="/fertibase">
+//     <App />
+//   </BrowserRouter>
+// );
+
+
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/fertibase">
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 );
+
